856f669ada91585d9ea43f9520fb77d1a69cc928
cameron.kennedy <cameron.kennedy@my.tccd.edu>
2018-10-03 17:43:09 -0500

    rename Pa2 to LispEvaluator

--- Programming Assignments/PA2/src/LispEvaluator/LispEvaluatorMain.java
+++ Programming Assignments/PA2/src/LispEvaluator/LispEvaluatorMain.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pa2;
+package LispEvaluator;
 
 import customconsole.JavaConsole;
 import java.util.logging.Level;
